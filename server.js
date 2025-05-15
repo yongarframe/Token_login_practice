23a88d4593818e4a53474530cbce73428cbb132a
yongarframe <yongar2002@gmail.com>
2025-05-15 12:07:41 +0900

    token 과제 완료

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://127.0.0.1:[본인의 포트번호]",
-      "http://localhost:[본인의 포트번호]",
-    ],
+    origin: ["http://127.0.0.1:5501", "http://localhost:5501"],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
     credentials: true,
   })
@@ -40,6 +37,11 @@ app.post("/", (req, res) => {
   if (!userInfo) {
     res.status(401).send("로그인 실패");
   } else {
+    const accessToken = jwt.sign({ userId: userInfo.user_id }, secretKey, {
+      expiresIn: 1000 * 60 * 5,
+    });
+    // console.log(accessToken);
+    res.send(accessToken);
     // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
     // 이곳에 코드를 작성하세요.
     // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
@@ -49,6 +51,12 @@ app.post("/", (req, res) => {
 
 // 클라이언트에서 get 요청을 받은 경우
 app.get("/", (req, res) => {
+  const accessToken = req.headers.authorization.split(" ")[1];
+  const payload = jwt.verify(accessToken, secretKey);
+
+  const userInfo = users.find((el) => el.user_id === payload.userId);
+  const { user_id, user_name, user_info } = userInfo;
+  return res.json({ user_id, user_name, user_info });
   // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
   // 이곳에 코드를 작성하세요.
   // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
